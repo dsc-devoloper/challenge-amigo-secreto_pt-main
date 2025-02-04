a022e4f63c6600991757b46084fc8eea0a6daa58
dsc-developer <dscprofissional2019@uni9.edu.br>
2025-02-03 22:43:13 -0300

    implementacao funcao agregar amigos

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 
+function adicionarAmigo() {
+    let adicionar = document.querySelector("input").value.trim(); // Remove espaços extras
 
+    if (adicionar !== "") { // Verifica se o campo não está vazio
+        listaAmigos.push(adicionar); // Adiciona o nome ao início do array
+    } else{
+        alert ("Por favor, insira um nome.");
+    }
+
+    limparCampo();
+    console.log(listaAmigos);
+}
+adicionarAmigo()
+
+function limparCampo() {
+    adicionar = document.querySelector('input');
+    adicionar.value = '';
+}
 
